56583e6887dce88be17f6f1ced324259a0f09728
Mohit <mohitt.tech@gmail.com>
2025-11-08 21:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 import time
 
 # User input
-second = int(input('Enter seconds :'))
+seconds = int(input('Enter seconds :'))
 
 # Main logic
-while second > 0:
-    mins = second / 60
-    sec = second % 60
+while seconds > 0:
+    mins = seconds / 60
+    sec = seconds % 60
     timer = f"{mins:02d}:{sec:02d}"
     print(timer, end="\r")
     time.sleep(1)
